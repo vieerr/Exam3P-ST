97608c107dd07bce3daf0ffe71e9ef323c56c165
vieerr <olivier_paspuel@hotmail.com>
2026-02-10 12:02:00 -0500

    fix: 🐛 adding intentional bug

--- eslint.config.js
+++ eslint.config.js
@@ -11,6 +11,7 @@ module.exports = [
       'no-console': 'off',
     },
   },
+  // Olivier Paspuel
   {
     files: ['**/__tests__/**/*.js', '**/*.test.js'],
     languageOptions: {
--- src/utils/index.js
+++ src/utils/index.js
@@ -22,7 +22,7 @@ const toCelsius = (f) => {
 // Olivier Paspuel
 const toFahrenheit = (c) => {
     assertFiniteNumber(c);
-    const result = (c * (9 / 5)) + 32;
+    const result = (c * (9 / 5)) + 31;
     return roundTo(result, 1);
 };
 
